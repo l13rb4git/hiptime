b49d14c4a703c5b0d7c1aeab83ef9f0e173648b1
macunaima <gcgamer46@gmail.com>
2020-10-22 15:10:09 -0300

    before action to find task and show action added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -22,5 +26,9 @@ class TasksController < ApplicationController
 
     def task_params
       params.require(:task).permit(:title, :description)
+    end
+
+    def find_task
+      @task = Task.find(params[:id])
     end
 end
