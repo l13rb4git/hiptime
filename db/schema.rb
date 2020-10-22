c5e6689e2b3b325e47029407ce25d73142f3c4d4
macunaima <gcgamer46@gmail.com>
2020-10-22 16:28:40 -0300

    Associating the Tasks with the User who created it

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_185429) do
+ActiveRecord::Schema.define(version: 2020_10_22_191517) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
