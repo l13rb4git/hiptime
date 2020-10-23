e3bad3ddb98fe62f1b91fcc38a97d164f96c4ab0
macunaima <gcgamer46@gmail.com>
2020-10-23 12:51:48 -0300

    Added functionality to mark task as completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,6 +44,7 @@ class TasksController < ApplicationController
   end
 
   def complete
+    @task = Task.find(params[:id])
     @task.update_attribute(:completed_at, Time.now)
     redirect_to root_path
   end
